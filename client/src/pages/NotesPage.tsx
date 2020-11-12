7276be35a41f9eb678d75016b93f1e17a978694e
MaksymBoudnik <Maksym.Boudnik@icuc.social>
2020-11-12 13:27:08 +0200

    Add sass; Add notesPage and note components;

--- client/src/pages/NotesPage.tsx
+++ client/src/pages/NotesPage.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import GridLayout from "react-grid-layout";
+import "react-grid-layout/css/styles.css";
+
+// import "react-resizable/css/styles.css";
+import "../styles/NotesPageStyles.scss";
+import { on } from "cluster";
+import Note from "../components/Note";
+
+// const NotesPage = () => {
+//   return <h1>Notes Page</h1>;
+// };
+
+const renderTiles = (tiles: string[]) => {
+  return tiles.map((tile) => (
+    <div className="tada" key={tile}>
+      {tile}
+    </div>
+  ));
+};
 
 const NotesPage = () => {
-  return <h1>Notes Page</h1>;
+  const layout = [
+    { i: "a", x: 0, y: 0, w: 1, h: 2, isDraggable: true, isResizable: false },
+    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+    { i: "c", x: 4, y: 0, w: 1, h: 2 },
+  ];
+
+  const [canv, setCanv] = useState(layout);
+  const onLayoutChange = (value: any) => {
+    setCanv(value);
+  };
+
+  const getTiles = () => layout.map((obj) => obj.i);
+
+  return (
+    <>
+      <Note />
+
+      {/*<GridLayout*/}
+      {/*  className="layout"*/}
+      {/*  layout={layout}*/}
+      {/*  cols={12}*/}
+      {/*  rowHeight={30}*/}
+      {/*  width={1200}*/}
+      {/*  onLayoutChange={onLayoutChange}*/}
+      {/*>*/}
+      {/*  {renderTiles(getTiles())}*/}
+      {/*</GridLayout>*/}
+    </>
+  );
 };
 
 export default NotesPage;
